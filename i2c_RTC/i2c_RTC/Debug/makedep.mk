322ab730e083864a5487fe907bd6f3378da1bd1f
rareinator <morten.9ssen@gmail.com>
2020-10-28 13:16:00 +0100

    Everythin

---
+++ i2c_RTC/i2c_RTC/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+i2cLib\i2cmaster.S
+
+i2cLib\test_i2cmaster.c
+
+i2cLib\twimaster.c
+
+main.c
+
+setup\stdio_setup.c
+
--- i2c_RTC/i2c_RTC/main.c
+++ i2c_RTC/i2c_RTC/main.c
@@ -6,6 +6,19 @@
 #include <util/setbaud.h>
 #include <ctype.h>
 #include <i2cLib/i2cmaster.h>
+#include <util/delay.h>
+
+#define SLAVE_ADDRESS 0x68
+
+struct  DateTime {
+	char Year;
+	char Month;
+	char Day;
+	char Date;
+	char Hour;
+	char Minute;
+	char Second;
+};
 
 unsigned char USART_Receive(void)
 {
@@ -30,12 +43,60 @@ void USART_Init()
 	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00); // Set frame format: 8 bit data, 1 stop bit, 22.10.4
 }
 
+void DS3231_init()
+{
+	
+	
+}
+
+void DS3231_setDateTime(DateTime time)
+{
+	i2c_start(SLAVE_ADDRESS); //Addess 00H
+	i2c_write(0x00) // seconds to 0
+	i2c_write(0x00)// minutes
+	i2c_write(0x12)// hours 00 01 0010
+	i2c_write(0x05)// day 0000 0101 fredag
+	i2c_write(0x30)// date 0011 0000 den 30
+	i2c_write(0x10)// month 0001 0000 den 10
+	i2c_write(0x20)// year 0010 0000
+	i2c_stop();				
+}
+
+DateTime DS3231_getDateTime()
+{
+	i2c_start(SLAVE_ADDRESS);
+	DateTime result;
+	result.Second = i2c_read(1);
+	result.Minute = i2c_read(1);
+	result.Hour = i2c_read(1);
+	result.Day = i2c_read(1);
+	result.Date = i2c_read(1);
+	result.Month = i2c_read(1);
+	result.Year = i2c_read(0);
+	i2c_stop();
+	
+	return result;
+};
+
+
 int main(void)
 {	
 	USART_Init();
-	i2c_init();
-		
-
+	i2c_init(); // init I2C library
+	DS3231_init();
+	struct DateTime start;
+	start.Year = 2020;
+	start.Month = 10;
+	start.Day = 28;
+	start.Hour = 11;
+	start.Minute = 30;
+	start.Second = 00;
+	DS3231_setDateTime(start)
+	
 	while(1) {
+		_delay_ms(1000);
+		DateTime now = DS3231_getDateTime();
+		USART_Transmit(now.Second);
+			
 	}
 }
