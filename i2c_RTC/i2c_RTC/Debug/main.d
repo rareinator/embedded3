a7f502bf424ae44b9d27cfb3470bc322a59fd707
rareinator <morten.9ssen@gmail.com>
2020-10-29 10:02:45 +0100

    For nico

--- i2c_RTC/i2c_RTC/Debug/main.d
+++ i2c_RTC/i2c_RTC/Debug/main.d
@@ -15,7 +15,11 @@ main.d main.o: .././main.c \
  .././i2cLib/i2cmaster.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../setup/stdio_setup.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -50,3 +54,11 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../setup/stdio_setup.h:
--- i2c_RTC/i2c_RTC/main.c
+++ i2c_RTC/i2c_RTC/main.c
@@ -7,96 +7,80 @@
 #include <ctype.h>
 #include "i2cLib/i2cmaster.h"
 #include <util/delay.h>
+#include <stdio.h>
+#include "stdio_setup.h"
 
 #define SLAVE_ADDRESS 0xD0
 
+#define SECONDS 0x00
+
 typedef struct  {
-	char Year;
-	char Month;
-	char Day;
-	char Date;
-	char Hour;
-	char Minute;
-	char Second;
+	uint8_t Year;
+	uint8_t Month;
+	uint8_t Day;
+	uint8_t Date;
+	uint8_t Hour;
+	uint8_t Minute;
+	uint8_t Second;
 } DateTime;
 
-unsigned char USART_Receive(void)
-{
-	while (	!(UCSR0A & (1<<RXC0)));	// Wait for data to be received
-	char recivedChar = UDR0;
-	
-	return toupper(recivedChar); // Get and return received data from buffer
-}
-
-void USART_Transmit(unsigned char data)
-{
-	while (	!(UCSR0A & (1<<UDRE0)));
-	UDR0 = data;  // Put data into buffer, sends the data
-}
-
-void USART_Init()
-{
-	UBRR0H = UBRRH_VALUE; // set UART0 baud rate
-	UBRR0L = UBRRL_VALUE;
-
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0); // Enable Receiver and Transmitter, 22.10.3	
-	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00); // Set frame format: 8 bit data, 1 stop bit, 22.10.4
-}
-
-void DS3231_init()
-{
-	
-	
-}
-
-void DS3231_setDateTime(DateTime time)
+void DS3231_setDateTime(DateTime *time)
 {
 	i2c_start(SLAVE_ADDRESS+I2C_WRITE); //Address 00H
-	i2c_write(0x00); // seconds to 0
-	i2c_write(0x00); // minutes
-	i2c_write(0x12); // hours 00 01 0010
-	i2c_write(0x05); // day 0000 0101 fredag
-	i2c_write(0x30); // date 0011 0000 den 30
-	i2c_write(0x10); // month 0001 0000 den 10
-	i2c_write(0x20); // year 0010 0000
+	i2c_write(SECONDS);
+	//i2c_write(0x00); // seconds to 0
+	//i2c_write(0x00); // minutes
+	//i2c_write(0x12); // hours 00 01 0010
+	//i2c_write(0x05); // day 0000 0101 fredag
+	//i2c_write(0x30); // date 0011 0000 den 30
+	//i2c_write(0x10); // month 0001 0000 den 10
+	//i2c_write(0x20); // year 0010 0000
+	i2c_write(time->Second); // seconds to 0
+	i2c_write(time->Minute); // minutes
+	i2c_write(time->Hour); // hours 00 01 0010
+	i2c_write(time->Day); // day 0000 0101 fredag
+	i2c_write(time->Date); // date 0011 0000 den 30
+	i2c_write(time->Month); // month 0001 0000 den 10
+	i2c_write(time->Year); // year 0010 0000
 	i2c_stop();				
 }
 
-DateTime DS3231_getDateTime()
+void DS3231_getDateTime(DateTime *now)
 {
-	i2c_start(SLAVE_ADDRESS+I2C_READ);
-	DateTime result;
-	result.Second = i2c_read(1);
-	result.Minute = i2c_read(1);
-	result.Hour = i2c_read(1);
-	result.Day = i2c_read(1);
-	result.Date = i2c_read(1);
-	result.Month = i2c_read(1);
-	result.Year = i2c_read(0);
+	i2c_start(SLAVE_ADDRESS+I2C_WRITE);
+	i2c_write(SECONDS);
 	i2c_stop();
 	
-	return result;
+	i2c_start(SLAVE_ADDRESS+I2C_READ);
+	now->Second = i2c_read(1);
+	now->Minute = i2c_read(1);
+	now->Hour = i2c_read(1);
+	now->Day = i2c_read(1);
+	now->Date = i2c_read(1);
+	now->Month = i2c_read(1);
+	now->Year = i2c_read(0);
+	i2c_stop();
 };
 
 
 int main(void)
 {	
-	USART_Init();
-	i2c_init(); // init I2C library
-	DS3231_init();
+	UartInit();
+	i2c_init(); // init I2C lib
 	DateTime start;
-	start.Year = 2020;
+	start.Year = 20;
 	start.Month = 10;
-	start.Day = 28;
-	start.Hour = 11;
+	start.Day = 5;
+	start.Date = 28;
+	start.Hour = 12;
 	start.Minute = 30;
 	start.Second = 00;
-	DS3231_setDateTime(start);
+	DS3231_setDateTime(&start);
 	
 	while(1) {
 		_delay_ms(1000);
-		DateTime now = DS3231_getDateTime();
-		USART_Transmit(now.Second);
-			
+		DateTime now;
+		DS3231_getDateTime(&now);
+		printf("Time: %d:%d:%d on %d:%d:20%d \n", now.Hour, now.Minute, now.Second, now.Date, now.Month, now.Year);
 	}
 }
