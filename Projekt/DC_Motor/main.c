d54500b86238ee40c8586645e6c406943a93ebe5
Kevin Pike Darmer <amopeika@gmail.com>
2020-11-02 09:43:04 +0100

    DC_Motor added to Project

---
+++ Projekt/DC_Motor/main.c
@@ -0,0 +1,18 @@
+/*
+ * DC_Motor.c
+ *
+ * Created: 02-11-2020 09:42:00
+ * Author : ASUS
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
