f97015e887ddf0616791d4661d4a9c705434aa4a
Nicolai Bjerg <looren@juel-kroyer.dk>
2020-11-02 11:03:41 +0100

    New exe project with DHT11 Library

---
+++ Projekt/DHT11lib/DHT11library.h
@@ -0,0 +1,34 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 02/11/2020 10:59:03
+ *  Author: Looren
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+#include <avr/io.h>
+
+typedef struct {
+	uint8_t Temperatur;
+	uint8_t Humidity;
+}DHT11;
+
+#define SET_BIT(port, bit) ((port) |= (1<<bit))
+#define CLR_BIT(port, bit) ((port) &= ~((1<<bit)))
+
+#define DHT11_DDR DDRB
+#define DHT11_PORT PORTB
+#define DHT11_PIN PINB
+#define DHT11_BIT PB3
+
+void DHT11_init();
+void DHT11_ReadRaw(DHT11 *result);
+uint8_t DHT11_ReadTemp();
+uint8_t DHT11_ReadHumid();
+
+
+
+#endif /* INCFILE1_H_ */
---
+++ Projekt/ExeProject/main.c
@@ -0,0 +1,19 @@
+/*
+ * ExeProject.c
+ *
+ * Created: 02/11/2020 10:57:25
+ * Author : Looren
+ */ 
+
+#include <avr/io.h>
+#include "DHT11library.h"
+
+int main(void)
+{
+	DHT11_ReadTemp();
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
