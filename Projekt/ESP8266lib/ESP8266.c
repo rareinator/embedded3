72325bf48db45107cb353815bceef077f5bac6bf
rareinator <morten.9ssen@gmail.com>
2020-11-03 08:57:01 +0100

    Added ESP8266 lib

---
+++ Projekt/ESP8266lib/ESP8266.c
@@ -0,0 +1,16 @@
+/*
+ * ESP8266lib.c
+ *
+ * Created: 11/3/2020 8:54:52 AM
+ * Author : rare
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Projekt/ESP8266lib/ESP8266.h
@@ -0,0 +1,16 @@
+/*
+ * ESP8266.h
+ *
+ * Created: 11/3/2020 8:55:26 AM
+ *  Author: rare
+ */ 
+
+
+#ifndef ESP8266_H_
+#define ESP8266_H_
+
+
+
+
+
+#endif /* ESP8266_H_ */
