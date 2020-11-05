d67f8d8b6da1a325f41ae322a10f349c414f4296
Nicolai Bjerg <looren@juel-kroyer.dk>
2020-11-05 08:42:00 +0100

    Test

--- Projekt/Master/main.c
+++ Projekt/Master/main.c
@@ -14,12 +14,12 @@ int main(void)
 	
 	SS_ENABLE;
 	
+	
+	SPI_Write(0); // Function Register: Register 0 = Turn On/Off - Register 1 = Fan speed
+	SPI_Write(1); // Register Value: If register 0 value = 0 or 1 = Off or On - If 1 value = 0-255 for dutycycle
+	
 	while(1)
 	{
-		SPI_Write(1);
-		SPI_Write(255);
-		SPI_Write(0); // Function Register: Register 0 = Turn On/Off - Register 1 = Fan speed 
-		SPI_Write(1); // Register Value: If register 0 value = 0 or 1 = Off or On - If 1 value = 0-255 for dutycycle
 	}
 }
 
