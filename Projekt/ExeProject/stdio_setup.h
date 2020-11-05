3be4760ead9e4b8fe01ab0ca2b4c3d7b9ea2ad2a
rareinator <morten.9ssen@gmail.com>
2020-11-05 11:22:03 +0100

    cleanup

--- Projekt/ExeProject/stdio_setup.h
+++
@@ -1,6 +0,0 @@
-#ifndef STDIO_SETUP_H_
-#define STDIO_SETUP_H_
-
-void UartInit(void);
-
-#endif /* STDIO_SETUP_H_ */
--- Projekt/Master/main.c
+++ Projekt/Master/main.c
@@ -35,3 +35,38 @@ int main(void)
 	}
 }
 
+
+
+//WIFI
+
+//char _buffer[150];
+//uint8_t Connect_Status;
+//
+//USART_Init(115200);						/* Initiate USART with 115200 baud rate */
+//sei();									/* Start global interrupt */
+//DHT11_init();                           /* Initialize DHT11 */
+//while(!ESP8266_Begin());
+//ESP8266_WIFIMode(BOTH_STATION_AND_ACCESPOINT); /* 3 = Both (AP and STA) */
+//ESP8266_ConnectionMode(MULTIPLE);			   /* 0 = Single; 1 = Multi */
+//ESP8266_StartServer(80);                       /* Start server mode with port 80 */
+////if(ESP8266_connected() == ESP8266_NOT_CONNECTED_TO_AP)
+////ESP8266_JoinAccessPoint(SSID, PASSWORD);
+////ESP8266_Start(0, DOMAIN, PORT);
+//while(1)
+//{
+	////Connect_Status = ESP8266_connected();
+	////if(Connect_Status == ESP8266_NOT_CONNECTED_TO_AP)
+	////ESP8266_JoinAccessPoint(SSID, PASSWORD);
+	////if(Connect_Status == ESP8266_TRANSMISSION_DISCONNECTED)
+	////ESP8266_Start(0, DOMAIN, PORT);
+	////
+	////DHT11 dhtData;
+	////DHT11_ReadRaw(&dhtData);
+	////
+	////memset(_buffer, 0, 150);
+	////sprintf(_buffer, "GET /update?api_key=%s&field1=%i&field2=%i", API_WRITE_KEY, dhtData.Temperatur, dhtData.Humidity);
+	////ESP8266_Send(_buffer);
+	////_delay_ms(15000);
+	//
+	//
+//}
